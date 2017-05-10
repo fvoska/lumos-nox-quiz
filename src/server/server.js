52aaa5ee198dd64fe989019e862bcc472448fdbd
Filip Voska <filipvoska@Filips-MacBook-Air.local>
2017-05-10 22:14:16 +0200

    Serve on Heroku

---
+++ src/server/server.js
@@ -0,0 +1,39 @@
+// server.js
+const express = require('express');
+const app = express();
+// If an incoming request uses
+// a protocol other than HTTPS,
+// redirect that request to the
+// same url but with HTTPS
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+// Instruct the app
+// to use the forceSSL
+// middleware
+//app.use(forceSSL());
+
+// Fix path
+const path = require('path');
+const splitPath = __dirname.split('/');
+splitPath.pop();
+splitPath.pop();
+const fixedPath = splitPath.join('/');
+
+// Serve client files.
+app.use(express.static(fixedPath + '/dist/client'));
+
+// For refreshing.
+app.use(function(req, res) {
+  // Use res.sendfile, as it streams instead of reading the file into memory.
+  res.sendFile(fixedPath + '/dist/client/index.html');
+});
+
+app.listen(process.env.PORT || 8080);
